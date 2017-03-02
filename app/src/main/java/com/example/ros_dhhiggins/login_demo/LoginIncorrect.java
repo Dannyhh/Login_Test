8f15fa1d7a3a3b4eae6724255b9e456e4de69f4d
Dannyhh <dobbykiko@gmail.com>
2017-03-02 08:54:10 -0800

    Added functionality to the back button on the incorrect login screen.

--- app/src/main/java/com/example/ros_dhhiggins/login_demo/LoginIncorrect.java
+++ app/src/main/java/com/example/ros_dhhiggins/login_demo/LoginIncorrect.java
@@ -1,7 +1,9 @@
 package com.example.ros_dhhiggins.login_demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginIncorrect extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoginIncorrect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_incorrect);
+    }
+
+    public void back(View view){
+        Intent goBack = new Intent(this, LoginScreen.class);
+            startActivity(goBack);
     }
 }
